626047e0d265bedc733639788ef0cc8ed0785c85
Dreammall <aimi6827@gmail.com>
2025-12-07 17:18:29 +0900

    css,jsの読み込みを定義

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+// resources/js/app.js
+
+// bootstrap 読み込み
 import './bootstrap';
+
+// CSSまとめ読み込み
+import '../css/style.css';
+import '../css/menu-style.css';
+import '../css/fade-in.css';
+
+// menu.js 読み込み
+import './menu';
